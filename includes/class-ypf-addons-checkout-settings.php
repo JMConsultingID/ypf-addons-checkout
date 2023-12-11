18d390f6053d9c4492422d76d746a682c79c9b70
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-11 15:15:37 +0800

    Update class-ypf-addons-checkout-settings.php

--- includes/class-ypf-addons-checkout-settings.php
+++ includes/class-ypf-addons-checkout-settings.php
@@ -33,7 +33,6 @@ class YPF_Addons_Checkout_Settings {
     public function options_page() {
         ?>
         <div class="wrap">
-            <h1>YPF Addons Settings</h1>
             <form method="post" action="options.php">
                 <?php
                 settings_fields('ypf_addons_options_group');
