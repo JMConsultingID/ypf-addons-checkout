e536b249cf656e4fc990c842838b1c76e8832274
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-11 15:12:51 +0800

    Update class-ypf-addons-checkout-settings.php

--- includes/class-ypf-addons-checkout-settings.php
+++ includes/class-ypf-addons-checkout-settings.php
@@ -3,7 +3,31 @@
 class YPF_Addons_Checkout_Settings {
 
     public function __construct() {
-        // Constructor code here.
+        add_action('admin_init', array($this, 'init_settings'));
+    }
+
+    public function init_settings() {
+        register_setting('ypf_addons_options_group', 'ypf_addons_enabled');
+
+        add_settings_section(
+            'ypf_addons_settings_section', 
+            'General Settings', 
+            null, 
+            'ypf-addons-checkout'
+        );
+
+        add_settings_field(
+            'ypf_addons_enabled', 
+            'Enable YPF Addons', 
+            array($this, 'ypf_addons_enabled_callback'), 
+            'ypf-addons-checkout', 
+            'ypf_addons_settings_section'
+        );
+    }
+
+    public function ypf_addons_enabled_callback() {
+        $option = get_option('ypf_addons_enabled');
+        echo '<input type="checkbox" id="ypf_addons_enabled" name="ypf_addons_enabled" value="1" ' . checked(1, $option, false) . '>';
     }
 
     public function options_page() {
@@ -20,6 +44,4 @@ class YPF_Addons_Checkout_Settings {
         </div>
         <?php
     }
-
-    // Other methods for settings.
 }
