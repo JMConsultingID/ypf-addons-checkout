d4cd16941e398785b016690fa91087a50f00034a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-11 15:23:59 +0800

    Update class-ypf-addons-checkout.php

--- includes/class-ypf-addons-checkout.php
+++ includes/class-ypf-addons-checkout.php
@@ -57,12 +57,67 @@ class YPF_Addons_Checkout {
             <h1>YPF Addons Checkout Settings</h1>
             <form method="post" action="options.php">
                 <?php
-                settings_fields( 'ypf_addons_group' );
-                do_settings_sections( 'ypf-addons-checkout' );
+                settings_fields( 'ypf_addons_option_group' );
+                do_settings_sections( 'ypf-addons-checkout-admin' );
                 submit_button();
                 ?>
             </form>
         </div>
+        <?php
+    }
+
+    /**
+     * Register and add settings.
+     */
+    public function page_init() {
+        register_setting(
+            'ypf_addons_option_group', // Option group
+            'ypf_addons_option_name',  // Option name
+            array( $this, 'sanitize' ) // Sanitize
+        );
+
+        add_settings_section(
+            'setting_section_id',        // ID
+            'Settings',                  // Title
+            array( $this, 'print_section_info' ), // Callback
+            'ypf-addons-checkout-admin'  // Page
+        );  
+
+        add_settings_field(
+            'enable',                    // ID
+            'Enable',                    // Title 
+            array( $this, 'enable_callback' ),   // Callback
+            'ypf-addons-checkout-admin', // Page
+            'setting_section_id'         // Section           
+        );      
+    }
+
+    /**
+     * Sanitize each setting field as needed.
+     */
+    public function sanitize( $input ) {
+        $new_input = array();
+        if( isset( $input['enable'] ) )
+            $new_input['enable'] = absint( $input['enable'] );
+
+        return $new_input;
+    }
+
+    /**
+     * Print the Section text.
+     */
+    public function print_section_info() {
+        print 'Change the settings below:';
+    }
+
+    /**
+     * Get the settings option array and print one of its values.
+     */
+    public function enable_callback() {
+        $options = get_option( 'ypf_addons_option_name' );
+        ?>
+        <input type="checkbox" id="enable" name="ypf_addons_option_name[enable]" value='1' <?php checked( 1, isset( $options['enable'] ) ? $options['enable'] : 0 ); ?> />
+        <label for="enable">Enable YPF Addons Checkout</label>
         <?php
     }
 
