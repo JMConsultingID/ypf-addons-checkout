5a77ae745902e7408472812bf1f316d3897fdc4a
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-12 14:52:55 +0800

    Update ypf-addons-checkout-widget.php

--- includes/widgets/ypf-addons-checkout-widget.php
+++ includes/widgets/ypf-addons-checkout-widget.php
@@ -27,9 +27,11 @@ class YPF_Addons_Checkout_Widget extends \Elementor\Widget_Base {
         $addons = $this->get_addons_data();
         if ( ! empty( $addons ) ) {
             echo '<form>';
+            $chosen_addon_id = WC()->session->get('chosen_addon');
             foreach ( $addons as $addon ) {
+                $is_checked = ($addon->id == $chosen_addon_id) ? 'checked' : '';
                 echo '<label>';
-                echo '<input type="radio" name="ypf_addon" value="' . esc_attr( $addon->id ) . '" data-value="' . $addon->value_percentage. '">';
+                echo '<input type="radio" name="ypf_addon" value="' . esc_attr( $addon->id ) . '" data-value="' . $addon->value_percentage. '"' . $is_checked . '>';
                 echo esc_html( $addon->addon_name );
                 $display_percentage = (intval($addon->value_percentage) == floatval($addon->value_percentage)) ? intval($addon->value_percentage) : floatval($addon->value_percentage);
                 echo ' (+' . esc_html($display_percentage) . '%)';
