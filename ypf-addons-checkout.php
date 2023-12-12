9f3fc65cfbeafdce35ee3d73c0fdc6d389cebc03
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-12 12:00:12 +0800

    Update ypf-addons-checkout.php

--- ypf-addons-checkout.php
+++ ypf-addons-checkout.php
@@ -146,20 +146,29 @@ function ypf_addons_list_page(){
     // HTML Form
     ?>
     <div class="wrap">
-        <h1><?php echo $edit ? 'Edit' : 'Add New'; ?> Add-On</h1>
+        <h1><?php echo $is_editing ? 'Edit Add-On' : 'Add New Add-On'; ?></h1>
         <form method="post" action="">
-            <table class="form-table" style="border-collapse: collapse; width: 100%;">
+            <style>
+                .ypf-form-table { width: 100%; }
+                .ypf-form-table td { padding: 0; vertical-align: top; }
+                .ypf-form-table input[type=text],
+                .ypf-form-table input[type=number],
+                .ypf-form-table select { width: 100%; }
+                .ypf-form-table .button { width: auto; }
+                .ypf-form-table label { display: block; margin-bottom: 4px; }
+            </style>
+            <table class="ypf-form-table">
                 <tr>
-                    <td style="padding: 10px; border: none;">
-                        <label for="addon_name">Add-On Name:</label><br>
+                    <td>
+                        <label for="addon_name">Add-On Name:</label>
                         <input type="text" id="addon_name" name="addon_name" value="<?php echo esc_attr($addon_name); ?>" />
                     </td>
-                    <td style="padding: 10px; border: none;">
-                        <label for="value_percentage">Value (Percentage):</label><br>
-                        <input type="text" id="value_percentage" name="value_percentage" value="<?php echo esc_attr($value_percentage); ?>" />
+                    <td>
+                        <label for="value_percentage">Value (Percentage):</label>
+                        <input type="number" id="value_percentage" name="value_percentage" value="<?php echo esc_attr($value_percentage); ?>" step="0.01" min="0" max="100" />
                     </td>
-                    <td style="padding: 10px; border: none;">
-                        <?php submit_button($edit ? 'Update Add-On' : 'Add Add-On', 'primary', 'submit', false); ?>
+                    <td>
+                        <?php submit_button($is_editing ? 'Update Add-On' : 'Add Add-On', 'primary', 'submit', false); ?>
                     </td>
                 </tr>
             </table>
