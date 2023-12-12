f42ff689b689a9e583ec178266709ada1c0b8784
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2023-12-12 12:41:50 +0800

    update core

---
+++ includes/widgets/ypf-addons-checkout-widget.php
@@ -0,0 +1,46 @@
+<?php
+// ypf-addons-checkout-widget.php
+
+class YPF_Addons_Checkout_Widget extends \Elementor\Widget_Base {
+
+    public function get_name() {
+        return 'ypf_addons_checkout';
+    }
+
+    public function get_title() {
+        return __( 'YPF Addons Checkout', 'ypf-addons-checkout' );
+    }
+
+    public function get_icon() {
+        return 'fa fa-code';
+    }
+
+    public function get_categories() {
+        return [ 'ypf-addons-category' ];
+    }
+
+    protected function _register_controls() {
+        // Register widget controls here
+    }
+
+    protected function render() {
+        $addons = $this->get_addons_data();
+        if ( ! empty( $addons ) ) {
+            echo '<form>';
+            foreach ( $addons as $addon ) {
+                // Assume each addon has 'id', 'addon_name', and 'value_percentage' properties
+                echo '<label>';
+                echo '<input type="radio" name="ypf_addon" value="' . esc_attr( $addon->id ) . '"> ';
+                echo esc_html( $addon->addon_name );
+                echo ' (' . esc_html( $addon->value_percentage ) . '%)';
+                echo '</label><br>';
+            }
+            echo '</form>';
+        }
+    }
+
+    private function get_addons_data() {
+        global $wpdb;
+        return $wpdb->get_results( "SELECT * FROM " . YPF_ADDONS_TABLE_NAME );
+    }
+}
