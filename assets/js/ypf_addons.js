5250dee5da1ec783e9ca4aa9b697c3eedc31608f
Ketut Ardika <93236121+ketutardika@users.noreply.github.com>
2024-05-20 17:40:36 +0800

    Update ypf_addons.js

--- assets/js/ypf_addons.js
+++ assets/js/ypf_addons.js
@@ -2,6 +2,27 @@ jQuery(document).ready(function($) {
     // Store the last selected radio button
     var lastChecked = $('input[type="radio"][name="ypf_addon"]:checked')[0];
 
+    if (lastChecked) {
+        var addonId = $(lastChecked).val();
+        var addonPercentage = $(lastChecked).data('value');
+
+        // AJAX request to update session with the selected add-on
+        $.ajax({
+            type: 'POST',
+            url: ypf_addons_ajax.ajax_url,
+            data: {
+                action: 'update_selected_addon',
+                addon_id: addonId,
+                addon_percentage: addonPercentage,
+                nonce: ypf_addons_ajax.nonce
+            },
+            success: function(response) {
+                // Trigger an update in the checkout totals
+                $(document.body).trigger('update_checkout');
+            }
+        });
+    }
+
     $('input[type="radio"][name="ypf_addon"]').on('click', function() {
         if (this === lastChecked) {
             $(this).prop('checked', false);
